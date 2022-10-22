31d4a5b7e3b0141685eaf8b941b7a22920e895bb
John Martinez <johnmartinezolarte@gmail.com>
2022-10-22 01:01:08 -0500

    version-3

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const display=document.querySelector('.display');
 const operators=document.querySelectorAll('.btn');
 const numbers=document.querySelectorAll('.btn_number');
 
+let key=true, operation='', accumulated='';
+
 operators.forEach((x,i)=>{
     x.addEventListener('click', ()=>operate(i));
 });
@@ -11,7 +13,29 @@ numbers.forEach((x,i)=>{
     x.addEventListener('click', ()=>enterNumber(i));
 });
 
-function operate(i){
+function operate(e){
+    if(e>=0 && e<=3){
+        operation+=operators[e].textContent;
+        display.textContent=operation;
+    }else if(e===4){
+
+        if(operation===''){
+            display.textContent='Error'
+        }else{
+            accumulated=operation;
+            accumulated+=`=${eval(operation)}`;
+            display.textContent=accumulated;
+            /* operation='' */
+            key=false;
+        }
+
+
+        
+
+    }else{
+        operation=''
+        display.textContent=''
+    }
     
 
 };
@@ -20,9 +44,30 @@ function operate(i){
 
 
 
+function enterNumber(i){
+    if(key){
+        if(operation===''){
+            operation=numbers[i].value;
+            display.textContent=operation;
+        }else{
+            operation+=numbers[i].value;
+            display.textContent=operation;
+        }
+    }else{
+        operation='';
+        key=true;
+        operation=numbers[i].value;
+        display.textContent=operation;
+    }
+};
 
 
-sum.addEventListener('click', ()=>{
+
+
+
+
+
+/* sum.addEventListener('click', ()=>{
     operator.textContent='+';
     values[1].value=values[0].value;
     values[0].value='';
@@ -75,4 +120,4 @@ deleteAC.addEventListener('click', ()=>{
     display.innerText='';
     values[0].value='';
     values[1].value='';
-});
+}); */
